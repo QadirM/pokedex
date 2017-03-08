50007a6965e2cb3e592e7d7510c1c8a96fa4a0fa
qadir_mohammedali <a.mohammedali@outlook.com>
2017-03-08 14:33:58 -0800

    Create fetechAllPokemon action and util

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+
+window.fetchAllPokemon = fetchAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
+
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);

006e4244658b96142df63c1e1f3712dc586b2522
qadir_mohammedali <a.mohammedali@outlook.com>
2017-03-08 14:57:04 -0800

    Create store and pokemonReducer

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 window.fetchAllPokemon = fetchAllPokemon;
 window.receiveAllPokemon = receiveAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+
+  window.store = store;
 });

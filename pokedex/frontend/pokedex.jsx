ca15f30429312623fa920ba5ec1fbd47672951de
qadir_mohammedali <a.mohammedali@outlook.com>
2017-03-08 16:25:47 -0800

    Create root component and updated podex entry file

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import configureStore from './store/store';
+import { selectAllPokemon } from './reducers/selectors';
+import Root from './components/root';
 
 
 window.fetchAllPokemon = fetchAllPokemon;
 window.receiveAllPokemon = receiveAllPokemon;
 window.requestAllPokemon = requestAllPokemon;
+window.selectAllPokemon = selectAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  ReactDOM.render(<Root store={store} />, rootEl);
 
   window.store = store;
 });
